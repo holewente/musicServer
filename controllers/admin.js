c4a3438af90aa258d9d6d087054c6dff47870c2c
Your Name <you@example.com>
2023-01-25 12:24:52 +0100

    'auth'

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,7 @@ import { configDB } from '../configDB.js';
 const db=mysql.createConnection(configDB)
 
 export const books=(request,response)=>{
-    db.query('SELECT * FROM books order by author',(err,result)=>{
+    db.query('SELECT * FROM musiclist order by username',(err,result)=>{
         if(err)
             console.log('hiba:',err)
         else
