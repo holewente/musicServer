b1553bc925baea96e7199262326309ac640ca795
Levi <you@example.com>
2023-04-28 08:52:27 +0200

    felhős_adatbázis

--- configDB.js
+++ configDB.js
@@ -1,6 +1,15 @@
-export const configDB={
+/*export const configDB={
     host :"localhost",
     user : "root",
     password : "",
     database :"musiclist"
+}*/
+import * as dotenv from 'dotenv';
+dotenv.config();
+export const configDB={
+    host     : process.env.MYSQL_HOST,
+    user     : process.env.MYSQL_USERNAME,
+    password : process.env.MYSQL_PASSWORD,
+    database : process.env.MYSQL_DATABASE
 }
+console.log("database:",configDB.host)
